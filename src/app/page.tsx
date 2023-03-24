38dc938aae0b4a928f3e71657e80338528e2dae6
365support <86206374+365support@users.noreply.github.com>
2023-03-24 11:15:05 +0900

    feat: featuredPosts 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Hero from "@/components/Hero";
+import FeaturedPosts from "@/components/FeaturedPosts";
 
 export default function HomePage() {
 	return (
 		<section>
 			<Hero />
+			{/* @ts-expect-error Server Component */}
+			<FeaturedPosts />
 		</section>
 	);
 }
---
+++ src/components/FeaturedPosts.tsx
@@ -0,0 +1,14 @@
+import { getFeaturedPosts } from "@/service/posts";
+import PostsGrid from "./PostsGrid";
+
+const FeaturedPosts = async () => {
+	const posts = await getFeaturedPosts();
+	return (
+		<section>
+			<h2 className="text-2xl font-bold my-2">Featured Posts</h2>
+			<PostsGrid posts={posts} />
+		</section>
+	);
+};
+
+export default FeaturedPosts;
