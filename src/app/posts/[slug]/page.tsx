2fd22b14de7b591d52c70ff29f6bd002b96b820b
365support <86206374+365support@users.noreply.github.com>
2023-03-28 15:53:31 +0900

    feat: 마크다운 뷰어 style, syntax-highlighter 구현

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import { getPostData } from "@/service/posts";
+import Image from "next/image";
+import PostContent from "@/components/PostContent";
 
 type Props = {
 	params: {
@@ -8,12 +10,19 @@ type Props = {
 
 const Postpage = async ({ params: { slug } }: Props) => {
 	const post = await getPostData(slug);
+	const { title, path } = post;
 
 	return (
-		<>
-			<h1>{post.title}</h1>
-			<pre>{post.content}</pre>
-		</>
+		<article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
+			<Image
+				className="rounded-2xl w-full h-1/5 max-h-[500px]"
+				src={`/images/posts/${path}.png`}
+				alt={title}
+				width={760}
+				height={420}
+			/>
+			<PostContent post={post} />
+		</article>
 	);
 };
 
